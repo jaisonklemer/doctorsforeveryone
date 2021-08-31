e1dc4a370dd395ff27241f68d4985320e2a4df4a
botaoap <gabrielbotaoap@gmail.com>
2021-08-31 09:08:45 -0300

    PFS-9 #comment Create Layout of Create Account to App

--- app/src/main/java/com/klemer/doctorapp/MainActivity.kt
+++ app/src/main/java/com/klemer/doctorapp/MainActivity.kt
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.klemer.doctorapp.utils.replaceView
 import com.klemer.doctorapp.view.SignInFragment
+import com.klemer.doctorapp.view.SignUpFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
