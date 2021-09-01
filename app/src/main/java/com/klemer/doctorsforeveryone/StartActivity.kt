fb590673d69da7c8f0b2dd802c448a7c08f73148
gabitheiss <t_gaaby@hotmail.com>
2021-09-01 14:51:30 -0300

    PFS-14 #comment Logo inserida

---
+++ app/src/main/java/com/klemer/doctorsforeveryone/StartActivity.kt
@@ -0,0 +1,11 @@
+package com.klemer.doctorsforeveryone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
