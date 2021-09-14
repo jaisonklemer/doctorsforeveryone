b39db44edc3ea62ca289b71eaf60dcb73ae76fb3
botaoap <gabrielbotaoap@gmail.com>
2021-09-14 09:25:36 -0300

    PFS-34 #comment Create function to Hide keyboard

--- app/src/main/java/com/klemer/doctorsforeveryone/utils/Extension.kt
+++ app/src/main/java/com/klemer/doctorsforeveryone/utils/Extension.kt
@@ -1,5 +1,9 @@
 package com.klemer.doctorsforeveryone.utils
 
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -9,4 +13,13 @@ fun FragmentActivity.replaceView(fragment : Fragment, @IdRes containerId: Int = 
     supportFragmentManager.beginTransaction()
         .replace(containerId, fragment)
         .commitNow()
+}
+
+fun FragmentActivity.hideKeyboard() {
+    hideKeyboard(currentFocus ?: View(this))
+}
+
+fun Context.hideKeyboard(view: View) {
+    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 }
